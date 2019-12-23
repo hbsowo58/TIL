ab8e4304d6138f8678f80f0ee190e872430dbd32
hbs <hbsowo58@gmail.com>
2019-12-23 22:06:15 +0900

    feat : add nspaceByx3 function

--- data structure & algorithm/number/nspacedByx.js
+++ data structure & algorithm/number/nspacedByx.js
@@ -17,6 +17,12 @@ function nspacedByx2(x, n) {
 
 // 불필요한 변수 제거
 
+function nspacedByx3(x, n) {
+  return Array(n).fill(x).map((v, i) => (i + 1) * v)
+}
+
+// n 개만큼 배열을 만든 후 일단 x로 초기화한 뒤돌면서 인덱스마다 인덱스 + 1 * 자릿값만큼 해준다
+
 console.log(nspacedByx(2, 5));
 console.log(nspacedByx(4, 3));
 console.log(nspacedByx(-4, 2));
@@ -24,3 +30,7 @@ console.log(nspacedByx(-4, 2));
 console.log(nspacedByx2(2, 5));
 console.log(nspacedByx2(4, 3));
 console.log(nspacedByx2(-4, 2));
+
+console.log(nspacedByx3(2, 5));
+console.log(nspacedByx3(4, 3));
+console.log(nspacedByx3(-4, 2));
