4b04c135b93d72f897861fb858c8caa2b1cd0894
hbs <hbsowo58@gmail.com>
2020-01-04 21:31:40 +0900

    feat: add hateSameNumber2 function use filter method

--- data structure & algorithm/number/hateSameNumber.js
+++ data structure & algorithm/number/hateSameNumber.js
@@ -1,4 +1,3 @@
-/* eslint-disable indent */
 function hateSameNumber(arr) {
   const answer = [];
   for (let i = 0; i < arr.length; i++) {
@@ -7,9 +6,15 @@ function hateSameNumber(arr) {
   return answer;
 }
 
-//반복문을 돌면서 뒷 인덱스랑 비교해서 답배열에 푸쉬
+// 반복문을 돌면서 다음 인덱스랑 비교해서 답 배열에 푸시
 
+function hateSameNumber2(arr) {
+  return arr.filter((ele, i) => ele !== arr[i + 1]);
+}
 
+// 배열의 고차 함수 사용, 각 인덱스와 그다음 인덱스를 비교해서 같지 않은 애들만 새로 만들어 리턴
 
 console.log(hateSameNumber([1, 1, 3, 3, 0, 1, 1]));
 console.log(hateSameNumber([4, 4, 4, 3, 3]));
+console.log(hateSameNumber2([1, 1, 3, 3, 0, 1, 1]));
+console.log(hateSameNumber2([4, 4, 4, 3, 3]));
