9ab798d72b8f18b7f18bba4e29ebf77c33a29db7
hbs <hbsowo58@gmail.com>
2020-03-01 14:15:53 +0900

    feat: add sortby function

---
+++ data structure & algorithm/Array/ArrayHof/test3.js
@@ -0,0 +1,51 @@
+const todos = [
+  {
+    id: 3,
+    content: "HTML",
+    completed: false,
+  },
+  {
+    id: 2,
+    content: "CSS",
+    completed: true,
+  },
+  {
+    id: 1,
+    content: "Javascript",
+    completed: false,
+  },
+];
+
+function sortBy(key) {
+  let temparr = [];
+  temparr = todos.slice();
+  return temparr.sort((a, b) =>
+    a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0
+  ); // 1자리는 양수로 가능
+  // return todos.slice().sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+}
+
+console.log(sortBy("id"));
+/*
+[
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false }
+]
+*/
+console.log(sortBy("content"));
+/*
+[
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+console.log(sortBy("completed"));
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true }
+]
+*/
