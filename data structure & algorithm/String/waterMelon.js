b767cafbd6cb6d67a957e58f43bb5cfd72e64797
hbs <hbsowo58@gmail.com>
2020-07-02 14:39:31 +0900

    feat : add waterMelon2 func use str.repeat method so improved readability

--- data structure & algorithm/String/waterMelon.js
+++ data structure & algorithm/String/waterMelon.js
@@ -1,4 +1,4 @@
-function waterMelon(n) {
+function waterMelon1(n) {
   let answer = "";
   const singleString = "수";
   const doubleString = "수박";
@@ -15,10 +15,21 @@ function waterMelon(n) {
 
   return answer;
 }
+// n이 들어온 숫자를 홀짝 판단하여 answer에 누적시키는 로직을 작성하였는데
+// string.repeat 메 서드를 사용하면 성능을 높일 수 있을 것 같다.
+// 사용법 str.repeat(count); 반복할 횟수
 
-console.log(waterMelon(0));
-console.log(waterMelon(1));
-console.log(waterMelon(2));
-console.log(waterMelon(3));
-console.log(waterMelon(4));
-console.log(waterMelon(5));
+function waterMelon2(n) {
+  const str = "수박";
+  return str.repeat(n / 2) + (n % 2 === 1 ? "수" : "");
+}
+
+// str.repeat(n / 2) 홀수 짝수를 구분하여 수를 추가하여 붙인다 repeat의 매개변수가 정수가 아니면 버림 발생.
+
+console.log(waterMelon1(0));
+console.log(waterMelon2(0));
+console.log(waterMelon2(1));
+console.log(waterMelon2(2));
+console.log(waterMelon2(3));
+console.log(waterMelon2(4));
+console.log(waterMelon2(5));
