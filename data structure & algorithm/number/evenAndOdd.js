53d4088a701c9fb7de9e61d44a104f8bb0588a52
hbs <hbsowo58@gmail.com>
2020-07-07 15:46:32 +0900

    feat: add evenAndOdd2 function

--- data structure & algorithm/number/evenAndOdd.js
+++ data structure & algorithm/number/evenAndOdd.js
@@ -4,6 +4,12 @@ function evenAndOdd(num) {
   return answer;
 }
 
+function evenAndOdd2(num) {
+  // if (num < 0) return false;
+  return num % 2 ? "Odd" : "Even";
+}
+// 0이 false라는 것을 이용한 한 줄로 가독성을 높임 0보다 작은 수를 false 처리하려고 했는데 검색 결과 음수도 홀, 짝이 구분된다
+
 console.log(evenAndOdd(0));
 console.log(evenAndOdd(1));
 console.log(evenAndOdd(2));
