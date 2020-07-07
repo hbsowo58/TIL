f6079732da5e075f376abae452f1a5deade713b0
hbs <hbsowo58@gmail.com>
2020-07-07 19:51:14 +0900

    feat : add stringToNumber function 3,4 use cast

--- data structure & algorithm/number/stringToNumber.js
+++ data structure & algorithm/number/stringToNumber.js
@@ -10,9 +10,22 @@ function stringToNumber2(s) {
 
 // 2와같이 변환되었는데 Math.floor처럼 동작하는 것 같다(Math.floor보단 느린듯)
 
+function stringToNumber3(str) {
+  return str / 1
+}
+// 1과 동일한 로직
 
+function stringToNumber4(str) {
+  return +str;
+}
+
+// 정수형을 문자열 연산을 원할때 + 덧셈연산자의 부수효과를 이용한다고 생각하였는데, 문자열에서 숫자형으로 변경할때도 되는 것 같다.
 
 console.log(stringToNumber('1234'))
 console.log(stringToNumber('-1234'))
 console.log(stringToNumber2('1234'))
 console.log(stringToNumber2('-1234'))
+console.log(stringToNumber3('1234'))
+console.log(stringToNumber3('-1234'))
+console.log(stringToNumber4('1234'))
+console.log(stringToNumber4('-1234'))
