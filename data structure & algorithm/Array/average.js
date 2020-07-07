96ead5d35af910c129b39f4cad55d06ed60e50c5
hbs <hbsowo58@gmail.com>
2020-07-07 15:53:03 +0900

    feat : add average2 function Improved readavility and performance throuth array high-order function reduce

--- data structure & algorithm/Array/average.js
+++ data structure & algorithm/Array/average.js
@@ -6,5 +6,10 @@ function average(arr) {
   return answer / arr.length;
 }
 
+function average2(array) {
+  return array.reduce((a, b) => a + b) / array.length;
+}
+//배열 고차함수 reduce를 통한 가독성 및 성능 향상
+
 console.log(average([1, 2, 3, 4]));
 console.log(average([5, 5]));
