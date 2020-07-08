b4504d0e69d022eb9d9ee23edeb6a83a26f29d5f
hbs <hbsowo58@gmail.com>
2020-07-08 16:17:56 +0900

    feat : add hideNumbers2 function solve widhout division using regular expressions

--- data structure & algorithm/String/hideNumbers.js
+++ data structure & algorithm/String/hideNumbers.js
@@ -1,17 +1,15 @@
-/* eslint-disable indent */
-/* eslint-disable no-unused-vars */
 function hideNumbers(phone_number) {
   const frontNum = phone_number.slice(0, phone_number.length - 4).replace(/(\d)/g, "*");
   const backNum = phone_number.slice(-4);
   return frontNum + backNum;
 }
 
-function hide_numbers2(s) {
+function hideNumbers2(s) {
   return s.replace(/\d(?=\d{4})/g, "*");
   // /(\d)/g 모든 숫자를 찾는것에서, (?=\d{4}) 하위식 추가 ?= 끝에서부터 4글자는 *로 대체하는 로직인듯(부채)
 }
 
 console.log(hideNumbers("01033334444"));
 console.log(hideNumbers("027778888"));
-console.log(hide_numbers2("01033334444"));
-console.log(hide_numbers2("027778888"));
+console.log(hideNumbers2("01033334444"));
+console.log(hideNumbers2("027778888"));
