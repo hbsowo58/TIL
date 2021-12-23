df223e0d18cc597cc89a8a58a45aae0eeca2fd18
hbs <hbsowo58@gmail.com>
2021-12-23 22:03:20 +0900

    fix : delete unnessary variable

--- data structure & algorithm/number/nspacedByx.js
+++ data structure & algorithm/number/nspacedByx.js
@@ -7,6 +7,20 @@ function nspacedByx(x, n) {
   return answer;
 }
 
+function nspacedByx2(x, n) {
+  let answer = [];
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+  return answer;
+}
+
+// 불필요한 변수 제거
+
 console.log(nspacedByx(2, 5));
 console.log(nspacedByx(4, 3));
 console.log(nspacedByx(-4, 2));
+
+console.log(nspacedByx2(2, 5));
+console.log(nspacedByx2(4, 3));
+console.log(nspacedByx2(-4, 2));
